a49ee97dffac55b99a91f04ba8494f5dc79ce5aa
Attidjany <appdev@prospective.group>
2025-09-28 20:16:31 +0000

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'expo-router/babel',
+      // Keep your alias here
       ['module-resolver', { alias: { '@': '.' } }],
     ],
   };
